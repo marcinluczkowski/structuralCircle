dfb00fa92a781b52857bd5fc580653944d29add3
Sigurlol <sigurlol@stud.ntnu.no>
2023-04-19 15:35:24 +0200

    Cleaning

--- Matching/matching.py
+++ Matching/matching.py
@@ -417,11 +417,9 @@ class Matching():
 
     # TODO (SIGURD) WORK IN PROGRESS: MAKING A NEW GENETIC ALGORITHM
     @_matching_decorator
-    def match_genetic_algorithm_RANDOM(self):
+    def match_genetic_algorithm(self):
         """Genetic algorithm with the initial population with random solutions (not necessary an actual solution)"""
-        #ASSUMING THAT WE WANT TO OPTIMIZE ON MINIMIZING LCA
         number_of_demand_elements = len(self.demand)
-        
         weights_new = hm.transform_weights(self.weights) #Create a new weight matrix with only one column representing all new elements
         weights_1d_array = weights_new.to_numpy().flatten()
         weights = np.array_split(weights_1d_array, number_of_demand_elements)
@@ -513,7 +511,7 @@ class Matching():
         """Parameters are set by use of trial and error. These parameters have given a satisfactory solution"""
         ga_instance = pygad.GA(
             initial_population=initial_population,
-            num_generations=int((len(self.demand)+len(self.supply))),
+            num_generations=int((len(self.demand) + len(self.supply))),
             num_parents_mating=int(np.ceil(solutions_per_population/2)),
             #fitness_func=fitness_func_matrix,
             fitness_func = fitness_func,
@@ -531,7 +529,7 @@ class Matching():
             mutation_by_replacement=True,
             random_mutation_min_val=0,
             random_mutation_max_val=1,   # upper bound exclusive, so only 0 and 1
-            save_best_solutions=True, #Needs a lot of memory
+            #save_best_solutions=True, #Needs a lot of memory
             )
 
         ga_instance.run()
@@ -540,90 +538,6 @@ class Matching():
         solution, solution_fitness, solution_idx = ga_instance.best_solution()
         extracted_results = hm.extract_genetic_solution(weights_new, solution, number_of_demand_elements)
         for index, row in extracted_results.iterrows():
-            self.add_pair(index, row["Matches from genetic"])
-    
-    @_matching_decorator
-    def match_genetic_algorithm_ALL_POSSIBILITIES(self):
-        """Genetic algorithm than only uses a subset of possible solutions as the initial population"""
-        #ASSUMING THAT WE WANT TO OPTIMIZE ON MINIMIZING LCA
-        number_of_demand_elements = len(self.demand)
-        weights_new = hm.transform_weights(self.weights) #Create a new weight matrix with only one column representing all new elements
-        weights_1d_array = weights_new.to_numpy().flatten()
-        weights = np.array_split(weights_1d_array, number_of_demand_elements)
-        max_weight = np.max(weights_1d_array[~np.isnan(weights_1d_array)])
-        supply_names = weights_new.columns
-    
-        #Initial population as a subset of actual possible solutions
-        new_sol = hm.create_initial_population_genetic(hm.transform_weights(self.incidence*1), size_of_population = len(supply_names)**2, include_invalid_combinations=False)
-
-
-        def fitness_func(solution, solution_idx):
-            """Fitness function to calculate fitness value of chromosomes
-            Genetic algorithm expects a maximization fitness function => when we are minimizing lca we must divide by 1/LCA"""
-            fitness = 0
-            reward = 0
-            solutions = np.array_split(solution, number_of_demand_elements)
-            penalty = -max_weight
-            indexes_of_matches = []
-            for i in range(len(solutions)):
-                num_matches_in_bracket = 0
-                for j in range(len(solutions[i])):
-                    if solutions[i][j] == 1:
-                        if np.isnan(weights[i][j]): #Element cannot be matched => penalty
-                            fitness += penalty #Penalty
-                        else:
-                            reward += weights[i][j] #LCA of match
-                            num_matches_in_bracket += 1
-                            new_element_index = len(solutions[i])-1
-                            if not j == new_element_index: #Means that a supply element (not a new element) is matched with a demand element
-                                indexes_of_matches.append(j)
-                            
-                if num_matches_in_bracket > 1:
-                    fitness += 10*penalty #Penalty for matching multiple supply elemenets to the same demand element
-                elif num_matches_in_bracket < 1:
-                    fitness += penalty #Penalty for not matching at all
-            
-            index_duplicates = {x for x in indexes_of_matches if indexes_of_matches.count(x) > 1}
-            if len(index_duplicates) > 0: #Means some supply elements are assigned the same demand element
-                fitness = -10e10
-            elif reward != 0:
-                fitness += 100/reward
-                  
-            return fitness
-        
-        def fitness_func_matrix(solution, solution_idx):
-            #TODO (SIGURD): Decrease run-time by doing matrix-multiplication to evaluate fitness of solution rather than double for-loop
-            fitness = 0
-            return fitness
-        
-        #TODO: Try parallaization
-        """Parameters are set by use of trial and error. These parameters have given a satisfactory solution"""
-        ga_instance = pygad.GA(
-            initial_population=new_sol,
-            num_generations=int((len(self.demand)+len(self.supply))),
-            num_parents_mating=int(np.ceil(len(new_sol)/2)),
-            fitness_func=fitness_func,
-            # binary representation of the problem with help from: https://blog.paperspace.com/working-with-different-genetic-algorithm-representations-python/
-            # (also possible with: gene_space=[0, 1])
-            gene_type=int,
-            parent_selection_type="sss",    # steady_state_selection() https://pygad.readthedocs.io/en/latest/README_pygad_ReadTheDocs.html#steady-state-selection
-            keep_elitism= int(np.ceil(len(new_sol)/4)),
-            #keep_parents=-1, #-1 => keep all parents, 0 => keep none
-            crossover_type="single_point", 
-            mutation_type = "random",
-            #mutation_num_genes=int(solutions_per_population/5), Not needed if mutation_probability is set
-            mutation_probability = 0.2,
-            mutation_by_replacement=True,
-            random_mutation_min_val=0,
-            random_mutation_max_val=1,   # upper bound exclusive, so only 0 and 1
-            )
-        ga_instance.run()
-        logging.debug(ga_instance.initial_population)
-        logging.debug(ga_instance.population)
-        solution, solution_fitness, solution_idx = ga_instance.best_solution()
-        extract_solution = hm.extract_genetic_solution(weights_new, solution, number_of_demand_elements)
-        printed_results = hm.print_genetic_solution(self.weights, solution, number_of_demand_elements)
-        for index, row in extract_solution.iterrows():
             self.add_pair(index, row["Matches from genetic"])
 
     @_matching_decorator
@@ -950,14 +864,8 @@ def run_matching(demand, supply, score_function_string_demand,score_function_str
         matching.match_scipy_milp()
         matches.append({'Name': 'Scipy_MILP','Match object': copy(matching), 'Time': matching.solution_time, 'PercentNew': matching.pairs.isna().sum()})
     if genetic:
-        #matching.match_genetic_algorithm()
-        #matches.append({'Name': 'Genetic','Match object': copy(matching), 'Time': matching.solution_time, 'PercentNew': matching.pairs.isna().sum()})
-
-        #matching.match_genetic_algorithm_ALL_POSSIBILITIES()
-        #matches.append({'Name': 'Genetic all possibilities','Match object': copy(matching), 'Time': matching.solution_time, 'PercentNew': matching.pairs.isna().sum()})
-
-        matching.match_genetic_algorithm_RANDOM()
-        matches.append({'Name': 'Genetic random','Match object': copy(matching), 'Time': matching.solution_time, 'PercentNew': matching.pairs.isna().sum()})
+        matching.match_genetic_algorithm()
+        matches.append({'Name': 'Genetic','Match object': copy(matching), 'Time': matching.solution_time, 'PercentNew': matching.pairs.isna().sum()})
     if brute:
         matching.match_brute()
         matches.append({'Name': 'Brute','Match object': copy(matching), 'Time': matching.solution_time, 'PercentNew': matching.pairs.isna().sum()})

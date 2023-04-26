a671460b31870197c984a50d05368a1adea573ab
Sigurlol <sigurlol@stud.ntnu.no>
2023-04-26 14:33:07 +0200

    Fixed to accept import from the xlsx-generated IFC files

--- TestCases/study_case_generate_report.py
+++ TestCases/study_case_generate_report.py
@@ -4,7 +4,6 @@ import pandas as pd
 sys.path.append('./Matching')
 import helper_methods as hm
 from matching import run_matching
-from GUI import run_GUI
 import LCA as lca
 
 
@@ -30,12 +29,13 @@ constants = {
     "Project name": "Bod nidarosdomen",
     "Metric": "GWP",
     "Algorithms": ["bipartite", "greedy_plural", "greedy_single", "bipartite_plural"],
-    "Include transportation": True,
+    "Include transportation": False,
     "Cite latitude": "63.4269",
     "Cite longitude": "10.3969",
-    "Demand file location": r"./CSV/pdf_demand.csv",
-    "Supply file location": r"./CSV/pdf_supply.csv",
-    "constraint_dict": {'Area' : '>=', 'Inertia_moment' : '>=', 'Length' : '>=', 'Material': '=='}
+    "Demand file location": r"./CSV/DEMAND_DATAFRAME_SVERRE.xlsx",
+    #"Supply file location": r"./CSV/SUPPLY_DATAFRAME_SVERRE.xlsx",
+    "Supply file location":"./CSV/pdf_supply.csv",
+    "constraint_dict": {'Area' : '>=', 'Moment of Inertia' : '>=', 'Length' : '>=', 'Material': '=='}
 }
 #========================#
 #Generating dataset
@@ -62,11 +62,30 @@ materials = ["Timber", "Steel"]
 #============
 #supply = hm.create_random_data_supply_pdf_reports(supply_count = 10, length_min = 1.0, length_max = 10.0, area_min = 0.15, area_max = 0.30, materials = materials, supply_coords = supply_coords)
 #demand = hm.create_random_data_demand_pdf_reports(demand_count = 10, length_min = 1.0, length_max = 10.0, area_min = 0.15, area_max = 0.30, materials = materials, demand_coords = demand_coords)
-#hm.export_dataframe_to_csv(supply, r"" + constants["Supply file location"])
-#hm.export_dataframe_to_csv(demand, r"" + constants["Demand file location"])
+#hm.export_dataframe_to_csv(supply, r"" + "./CSV/pdf_supply.csv")
+#hm.export_dataframe_to_csv(demand, r"" + "./CSV/pdf_demand.csv")
 #========================================
 
-hm.run_design_tool(constants)
+score_function_string = hm.generate_score_function_string(constants)
+#supply = hm.import_dataframe_from_csv(r"" + constants["Supply file location"])
+#demand = hm.import_dataframe_from_csv(r"" + constants["Demand file location"])
+supply = hm.import_dataframe_from_file(r"" + constants["Supply file location"], index_replacer = "S")
+demand = hm.import_dataframe_from_file(r"" + constants["Demand file location"], index_replacer = "D")
+
+#hm.create_graph(supply, demand, "Length", number_of_intervals= 5, save_filename = r"C:\Users\sigur\OneDrive - NTNU\Masteroppgave\Grafer_study_case")
+
+constraint_dict = constants["constraint_dict"]
+#Add necessary columns to run the algorithm
+supply = hm.add_necessary_columns_pdf(supply, constants)
+demand = hm.add_necessary_columns_pdf(demand, constants)
+run_string = hm.generate_run_string(constants)
+result = eval(run_string)
+simple_pairs = hm.extract_pairs_df(result)
+pdf_results = hm.extract_results_df_pdf(result, constants)
+pdf = hm.generate_pdf_report(pdf_results, filepath = r"./Results/")
+print(hm.extract_pairs_df(result))
+
+
 
 
 

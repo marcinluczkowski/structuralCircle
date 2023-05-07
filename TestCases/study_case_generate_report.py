b997ba38bda6b770e94cf7798c4092acfe844b34
Sigurlol <sigurlol@stud.ntnu.no>
2023-05-07 10:16:17 +0200

    Changed generate_report studycase

--- TestCases/study_case_generate_report.py
+++ TestCases/study_case_generate_report.py
@@ -13,30 +13,30 @@ import plotting as plot
 #Constants
 #TODO: FIND ALL DEFAULT VALUES FOR CONSTANTS, especially for price
 constants = {
-    "TIMBER_GWP": 28.9,       # based on NEPD-3442-2053-EN
-    "TIMBER_REUSE_GWP": 2.25,        # 0.0778*28.9 = 2.25 based on Eberhardt
-    "TRANSPORT_GWP": 96.0,    # TODO kg/m3/t based on ????
-    "TIMBER_DENSITY": 491.0,  # kg, based on NEPD-3442-2053-EN
-    "STEEL_GWP": 800, #Random value
-    "STEEL_REUSE_GWP": 4, #Random value
-    "VALUATION_GWP": 0.6, #In kr:Per kg CO2, based on OECD
-    "TIMBER_PRICE": 435, #Per m^3 https://www.landkredittbank.no/blogg/2021/prisen-pa-sagtommer-okte-20-prosent/
-    "TIMBER_REUSE_PRICE" : 100, #Per m^3, Random value
-    "STEEL_PRICE": 500, #Per m^2, Random value
-    "STEEL_REUSE_PRICE": 200, #Per m^2, Random value
-    "PRICE_TRANSPORTATION": 3.78, #Price per km per tonn. Derived from 2011 numbers on scaled t0 2022 using SSB
-    "STEEL_DENSITY": 7850,
+    "TIMBER_GWP": 28.9,       #kg CO2 eq per m^3, based on NEPD-3442-2053-EN
+    "TIMBER_REUSE_GWP": 2.25,        # 0.0778*28.9 = 2.25kg CO2 eq per m^3, based on Eberhardt
+    "TRANSPORT_GWP": 89.6,    #gram per tonne per km, Engedal et. al. 
+    "TIMBER_DENSITY": 491.0,  #kg/m^3, based on NEPD-3442-2053-EN
+    "STEEL_GWP": 9263.0, #kg CO2 eq per m^3, Norsk stål + density of steel
+    "STEEL_REUSE_GWP": 278.0, #kg CO2 eq per m^3, reduction of 97% from new according to Høydahl and Walter
+    "VALUATION_GWP": 0.7, #NOK per kg CO2, based on OECD
+    "TIMBER_PRICE": 3400.0, #Per m^3, Treindustrien 2023
+    "TIMBER_REUSE_PRICE" : 3400.0, #Per m^3, assumes the price is the same is new elements
+    "STEEL_PRICE": 67, #NOK per kg, ENTRA 2021
+    "STEEL_REUSE_PRICE": 100, #NOK per kg, ENTRA 2021
+    "PRICE_TRANSPORTATION": 0.3, #NOK per km per tonne, Grønland 2022 + Gran 2013
+    "STEEL_DENSITY": 7850, #kg/m^3 EUROCODE
     ########################
     "Project name": "Sognsveien 17",
-    "Metric": "GWP",
+    "Metric": "Price",
     "Algorithms": ["bipartite", "greedy_plural"],
-    "Include transportation": False,
+    "Include transportation": True,
     "Cite latitude": "59.94161606",
     "Cite longitude": "10.72994518",
-    #"Demand file location": r"./CSV/DEMAND_DATAFRAME_SVERRE.xlsx",
-    #"Supply file location": r"./CSV/SUPPLY_DATAFRAME_SVERRE.xlsx",
-    "Demand file location": r"./CSV/pdf_demand.csv",
-    "Supply file location": r"./CSV/pdf_supply.csv",
+    "Demand file location": r"./CSV/DEMAND_DATAFRAME_SVERRE.xlsx",
+    "Supply file location": r"./CSV/SUPPLY_DATAFRAME_SVERRE.xlsx",
+    #"Demand file location": r"./CSV/pdf_demand.csv",
+    #"Supply file location": r"./CSV/pdf_supply.csv",
     "constraint_dict": {'Area' : '>=', 'Moment of Inertia' : '>=', 'Length' : '>=', 'Material': '=='}
 }
 #========================#
@@ -71,33 +71,27 @@ materials = ["Timber", "Steel"]
 score_function_string = hm.generate_score_function_string(constants)
 supply = hm.import_dataframe_from_file(r"" + constants["Supply file location"], index_replacer = "S")
 demand = hm.import_dataframe_from_file(r"" + constants["Demand file location"], index_replacer = "D")
-
-#hm.create_graph(supply, demand, "Length", number_of_intervals= 2, save_filename = r"C:\Users\sigur\Downloads\test.png")
-
-#plot.create_graph(supply, demand, target_column="Length", unit=r"[m]", number_of_intervals=5, fig_title = "", save_filename=r"testplot2.png")
-#plot.create_graph(supply, demand, target_column="Area", unit=r"[m$^2$]", number_of_intervals=5, fig_title = "", save_filename=r"testplot3.png")
-#plot.create_graph(supply, demand, target_column="Moment of Inertia", unit=r"[m$^4$]", number_of_intervals=5, fig_title = "", save_filename=r"testplot4.png")
-
-
-constraint_dict = constants["constraint_dict"]
 #Add necessary columns to run the algorithm
 supply = hm.add_necessary_columns_pdf(supply, constants)
 demand = hm.add_necessary_columns_pdf(demand, constants)
-run_string = hm.generate_run_string(constants)
-result = eval(run_string)
-simple_pairs = hm.extract_pairs_df(result)
-pdf_results = hm.extract_results_df_pdf(result, constants)
-pdf = hm.generate_pdf_report(pdf_results, projectname = constants["Project name"], filepath = r"./Local_files/GUI_files/Results/")
-print(hm.extract_pairs_df(result))
 
+#plot.create_graph(supply, demand, target_column="Length", unit=r"[m]", number_of_intervals=5, fig_title = "", save_filename=r"length_plot.png")
+#plot.create_graph(supply, demand, target_column="Area", unit=r"[m$^2$]", number_of_intervals=5, fig_title = "", save_filename=r"area_plot.png")
+#plot.create_graph(supply, demand, target_column="Moment of Inertia", unit=r"[m$^4$]", number_of_intervals=5, fig_title = "", save_filename=r"inertia_plot.png")
 plot.plot_materials(supply, demand, "", save_filename=r"material_plot.png")
 
 
-#Generate map HTML
-plot.create_map_dataframe(demand, color = "red", legend_text="Manufacturer locations", save_name=r"map_manufacturer_test")
+constraint_dict = constants["constraint_dict"]
 
-
-
+run_string = hm.generate_run_string(constants)
+result = eval(run_string)
+""""""
+simple_pairs = hm.extract_pairs_df(result)
+pdf_results = hm.extract_results_df_pdf(result, constants)
+#plot.create_map_substitutions(supply, pdf_results, "supply", color = "green", legend_text="Substitution locations", save_name=r"map_reuse_subs")
+#plot.create_map_substitutions(demand, pdf_results, "demand", color = "red", legend_text="Manufacturer locations", save_name=r"map_manu_subs")
+#pdf = hm.generate_pdf_report(pdf_results, projectname = constants["Project name"], filepath = r"./Local_files/GUI_files/Results/")
+#print(hm.extract_pairs_df(result))
 
 
 
